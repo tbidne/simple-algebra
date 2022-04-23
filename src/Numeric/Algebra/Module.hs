227394af1a0fbeb75c33f9694d44deeb4d34dc9f
Tommy Bidne <tbidne@protonmail.com>
2022-04-23 23:53:46 +1200

    Add AGroup constraint to Module

--- src/Numeric/Algebra/Module.hs
+++ src/Numeric/Algebra/Module.hs
@@ -6,13 +6,14 @@ module Numeric.Algebra.Module
   )
 where
 
+import Numeric.Algebra.Additive.AGroup (AGroup)
 import Numeric.Algebra.Ring (Ring)
 import Numeric.Algebra.Semimodule (Semimodule (..))
 
 -- | Defines a module over a ring.
 --
 -- @since 0.1
-class (Semimodule m r, Ring r) => Module m r | m -> r
+class (AGroup m, Semimodule m r, Ring r) => Module m r | m -> r
 
 -- | @since 0.1
 instance Ring r => Module (r, r) r
