f09fdd3991b5442fe051673986394dcd4aee3853
Tommy Bidne <tbidne@protonmail.com>
2022-05-03 12:53:10 +1200

    Move abs from AMonoid to ASemigroup

--- src/Numeric/Algebra/Additive/ASemigroup.hs
+++ src/Numeric/Algebra/Additive/ASemigroup.hs
@@ -18,79 +18,100 @@ class Eq s => ASemigroup s where
   -- | @since 0.1
   (.+.) :: s -> s -> s
 
+  -- | @since 0.1
+  aabs :: s -> s
+
 infixl 6 .+.
 
 -- | @since 0.1
 instance ASemigroup Double where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup Float where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup Int where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup Int8 where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup Int16 where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup Int32 where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup Int64 where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup Integer where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup Word where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup Word8 where
+  aabs = abs
   (.+.) = (+)
 
 -- | @since 0.1
 instance ASemigroup Word16 where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup Word32 where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup Word64 where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup Natural where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup (Ratio Integer) where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup (Ratio Natural) where
   (.+.) = (+)
+  aabs = abs
 
 -- | @since 0.1
 instance ASemigroup a => ASemigroup (a, a) where
   (x1, x2) .+. (y1, y2) = (x1 .+. y1, x2 .+. y2)
+  aabs (x1, x2) = (aabs x1, aabs x2)
 
 -- | @since 0.1
 instance ASemigroup a => ASemigroup (a, a, a) where
   (x1, x2, x3) .+. (y1, y2, y3) = (x1 .+. y1, x2 .+. y2, x3 .+. y3)
+  aabs (x1, x2, x3) = (aabs x1, aabs x2, aabs x3)
 
 -- | @since 0.1
 instance ASemigroup a => ASemigroup (a, a, a, a) where
@@ -100,6 +121,7 @@ instance ASemigroup a => ASemigroup (a, a, a, a) where
       x3 .+. y3,
       x4 .+. y4
     )
+  aabs (x1, x2, x3, x4) = (aabs x1, aabs x2, aabs x3, aabs x4)
 
 -- | @since 0.1
 instance ASemigroup a => ASemigroup (a, a, a, a, a) where
@@ -110,6 +132,8 @@ instance ASemigroup a => ASemigroup (a, a, a, a, a) where
       x4 .+. y4,
       x5 .+. y5
     )
+  aabs (x1, x2, x3, x4, x5) = (aabs x1, aabs x2, aabs x3, aabs x4, aabs x5)
+
 
 -- | @since 0.1
 instance ASemigroup a => ASemigroup (a, a, a, a, a, a) where
@@ -120,6 +144,14 @@ instance ASemigroup a => ASemigroup (a, a, a, a, a, a) where
       x4 .+. y4,
       x5 .+. y5,
       x6 .+. y6
+    )
+  aabs (x1, x2, x3, x4, x5, x6) =
+    ( aabs x1,
+      aabs x2,
+      aabs x3,
+      aabs x4,
+      aabs x5,
+      aabs x6
     )
 
 -- | @since 0.1
@@ -132,6 +164,15 @@ instance ASemigroup a => ASemigroup (a, a, a, a, a, a, a) where
       x5 .+. y5,
       x6 .+. y6,
       x7 .+. y7
+    )
+  aabs (x1, x2, x3, x4, x5, x6, x7) =
+    ( aabs x1,
+      aabs x2,
+      aabs x3,
+      aabs x4,
+      aabs x5,
+      aabs x6,
+      aabs x7
     )
 
 -- | @since 0.1
@@ -146,6 +187,16 @@ instance ASemigroup a => ASemigroup (a, a, a, a, a, a, a, a) where
       x7 .+. y7,
       x8 .+. y8
     )
+  aabs (x1, x2, x3, x4, x5, x6, x7, x8) =
+    ( aabs x1,
+      aabs x2,
+      aabs x3,
+      aabs x4,
+      aabs x5,
+      aabs x6,
+      aabs x7,
+      aabs x8
+    )
 
 -- | @since 0.1
 instance ASemigroup a => ASemigroup (a, a, a, a, a, a, a, a, a) where
@@ -159,4 +210,15 @@ instance ASemigroup a => ASemigroup (a, a, a, a, a, a, a, a, a) where
       x7 .+. y7,
       x8 .+. y8,
       x9 .+. y9
+    )
+  aabs (x1, x2, x3, x4, x5, x6, x7, x8, x9) =
+    ( aabs x1,
+      aabs x2,
+      aabs x3,
+      aabs x4,
+      aabs x5,
+      aabs x6,
+      aabs x7,
+      aabs x8,
+      aabs x9
     )
