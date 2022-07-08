086da47c888d0662f4047ad04214df03097d3b08
Tommy Bidne <tbidne@protonmail.com>
2022-07-08 18:12:18 +1200

    Add stack versions to ci

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "algebra-simple flake";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=98000933d72a97632caf0db0027ea3eb2e5e7f29";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs =
     { flake-utils
@@ -10,7 +10,7 @@
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
-      compilerVersion = "ghc922";
+      compilerVersion = "ghc923";
       compiler = pkgs.haskell.packages."${compilerVersion}";
       mkPkg = returnShellEnv:
         compiler.developPackage {
