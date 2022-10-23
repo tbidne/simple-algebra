260ca109d4fc36e1a7a6fa4bff6d37cae2a661b4
Tommy Bidne <tbidne@protonmail.com>
2022-10-23 13:56:40 +1300

    Swap doctest for doctest-parallel
    Doctest-parallel seems less fragile (cannot get doctest to work by any
    means currently), and comes with the bonus that we do not have to manually
    specific cabal config.

--- test/doctest/Main.hs
+++ test/doctest/Main.hs
@@ -11,38 +11,5 @@ main =
   guardOrElse'
     "RUN_DOCTEST"
     ExpectEnvSet
-    (DT.doctest args)
+    (DT.mainFromCabal "algebra-simple" [])
     (putStrLn "*** Doctests Disabled ***")
-  where
-    args = files <> exts
-
-files :: [String]
-files =
-  [ "-isrc",
-    "src/Numeric/Data/NonZero.hs",
-    "src/Numeric/Algebra/Multiplicative/MGroup.hs"
-  ]
-
-exts :: [String]
-exts =
-  [ "-XNoStarIsType",
-    "-XBangPatterns",
-    "-XDataKinds",
-    "-XDeriveDataTypeable",
-    "-XDeriveGeneric",
-    "-XDeriveLift",
-    "-XDerivingStrategies",
-    "-XFlexibleContexts",
-    "-XFlexibleInstances",
-    "-XGeneralizedNewtypeDeriving",
-    "-XFunctionalDependencies",
-    "-XImportQualifiedPost",
-    "-XMultiParamTypeClasses",
-    "-XPatternSynonyms",
-    "-XRankNTypes",
-    "-XScopedTypeVariables",
-    "-XStandaloneKindSignatures",
-    "-XTypeApplications",
-    "-XTypeFamilies",
-    "-XTypeOperators"
-  ]
