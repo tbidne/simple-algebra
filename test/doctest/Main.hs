70b222f57e2ab4c64b2d47c12940bbd45e8ed914
Tommy Bidne <tbidne@protonmail.com>
2023-02-22 10:11:12 +1300

    Swap doctest-parallel back to doctest
    The new doctest-parallel is not behaving well with nix, despite its
    intention. Happily, the original doctest appears to work as long as
    we do the right configuration first.

--- test/doctest/Main.hs
+++ test/doctest/Main.hs
@@ -11,5 +11,38 @@ main =
   guardOrElse'
     "RUN_DOCTEST"
     ExpectEnvSet
-    (DT.mainFromCabal "algebra-simple" [])
+    (DT.doctest args)
     (putStrLn "*** Doctests Disabled ***")
+  where
+    args = files <> exts
+
+files :: [String]
+files =
+  [ "-isrc",
+    "src/Numeric/Data/NonZero.hs",
+    "src/Numeric/Algebra/Multiplicative/MGroup.hs"
+  ]
+
+exts :: [String]
+exts =
+  [ "-XNoStarIsType",
+    "-XBangPatterns",
+    "-XDataKinds",
+    "-XDeriveAnyClass",
+    "-XDeriveGeneric",
+    "-XDeriveLift",
+    "-XDerivingStrategies",
+    "-XFlexibleContexts",
+    "-XFlexibleInstances",
+    "-XGeneralizedNewtypeDeriving",
+    "-XFunctionalDependencies",
+    "-XImportQualifiedPost",
+    "-XMultiParamTypeClasses",
+    "-XPatternSynonyms",
+    "-XRankNTypes",
+    "-XScopedTypeVariables",
+    "-XStandaloneKindSignatures",
+    "-XTypeApplications",
+    "-XTypeFamilies",
+    "-XTypeOperators"
+  ]
