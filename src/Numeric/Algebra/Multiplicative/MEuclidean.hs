a0500e03954d658050126b089a1b6a5334ca3eee
Tommy Bidne <tbidne@protonmail.com>
2024-12-25 23:22:31 +1300

    Add gcd and lcm to MEuclidean
    Also remove default MEuclidean method that was impossible to use.

--- src/Numeric/Algebra/Multiplicative/MEuclidean.hs
+++ src/Numeric/Algebra/Multiplicative/MEuclidean.hs
@@ -3,7 +3,10 @@
 -- @since 0.1
 module Numeric.Algebra.Multiplicative.MEuclidean
   ( MEuclidean (..),
+    mdiv,
     mmod,
+    mgcd,
+    mlcm,
   )
 where
 
@@ -11,7 +14,10 @@ import Data.Int (Int16, Int32, Int64, Int8)
 import Data.Kind (Constraint, Type)
 import Data.Word (Word16, Word32, Word64, Word8)
 import GHC.Natural (Natural)
-import Numeric.Algebra.Multiplicative.MGroup (MGroup ((.%.)))
+import Numeric.Algebra.Additive.AMonoid (AMonoid (zero))
+import Numeric.Algebra.Multiplicative.MGroup (MGroup)
+import Numeric.Algebra.Multiplicative.MSemigroup ((.*.))
+import Numeric.Algebra.Normed (Normed (norm))
 
 -- | 'MGroup' equipped with "euclidean" division.
 --
@@ -20,13 +26,34 @@ type MEuclidean :: Type -> Constraint
 class (MGroup g) => MEuclidean g where
   -- | @since 0.1
   mdivMod :: g -> g -> (g, g)
-  mdivMod x d = (x .%. d, x `mmod` d)
-  {-# INLINE mdivMod #-}
+
+-- | @since 0.1
+mdiv :: (MEuclidean g) => g -> g -> g
+mdiv x d = fst $ mdivMod x d
+{-# INLINE mdiv #-}
 
 -- | @since 0.1
 mmod :: (MEuclidean g) => g -> g -> g
 mmod x d = snd $ mdivMod x d
 {-# INLINE mmod #-}
+
+-- | @since 0.1
+mgcd :: (AMonoid g, Eq g, MEuclidean g, Normed g) => g -> g -> g
+mgcd x y = gcd' (norm x) (norm y)
+  where
+    gcd' a b =
+      if b == zero
+        then a
+        else gcd' b (a `mmod` b)
+{-# INLINE mgcd #-}
+
+-- | @since 0.1
+mlcm :: (AMonoid g, Eq g, MEuclidean g, Normed g) => g -> g -> g
+mlcm x y
+  | x == zero = zero
+  | y == zero = zero
+  | otherwise = norm (x `mdiv` mgcd x y .*. y)
+{-# INLINE mlcm #-}
 
 -- | @since 0.1
 instance MEuclidean Int where
